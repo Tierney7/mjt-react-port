c4f0cb8039ae838163ee96a3c061416e1306d744
Matthew Tierney <tierneybbq@gmail.com>
2022-11-17 20:41:49 -0600

    about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import About from './components/About';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import ContactUs from './components/ContactUs';
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
 class App extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
+        <Contact resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
     );
